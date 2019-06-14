dd57d21a0c1ebe27f821cf6c5c4379cb2fb4b679
Tracie T <traciecodes@gmail.com>
2019-06-14 12:07:55 -0500

    finished obj-arrays and callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====  DONE
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
@@ -6,16 +6,28 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+// function consume(param1, param2, cb){
+//   return cb(param1, param2);
+// }
 
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+// function add(a, b) {
+//   return a+b;
+// }
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+// function multiply(a,b){
+//   return a*b;
+// }
 
+// function greeting(firstName, lastName){
+//   return `Hello ${firstName} ${lastName}, nice to meet you!`;
+// }
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
@@ -28,15 +40,15 @@
 // Explanation: 
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -56,51 +56,51 @@
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
-const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+// const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+// {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+// {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+// {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+// {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+// {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+// {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+// {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+// {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+// {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+// /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
+// Once you have the new array created, sort the universities alphabetically and log the result. */
 
-const universities = [];
+// const universities = [];
 
-for (i = 0; i < graduates.length; i++) {
-  graduates[i].university;
-  universities.push(graduates[i].university);
-  universities.sort();
-};
-console.log(universities);
+// for (i = 0; i < graduates.length; i++) {
+//   graduates[i].university;
+//   universities.push(graduates[i].university);
+//   universities.sort();
+// };
+// console.log(universities);
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+// /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
-The resulting contact information should have a space between the first name and the email information like this: 
-Name email@example.com
+// The resulting contact information should have a space between the first name and the email information like this: 
+// Name email@example.com
 
-Log the result of your new array. */
-const contactInfo = [];
-for (i = 0; i < graduates.length; i++) {
-  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
-}
-console.log(contactInfo);
+// Log the result of your new array. */
+// const contactInfo = [];
+// for (i = 0; i < graduates.length; i++) {
+//   contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+// }
+// console.log(contactInfo);
 
 
-// /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
-for (i = 0; i < graduates.length; i++) {
-  if (graduates[i].university.toLowerCase().includes('Uni')) {
-      uni.push(graduates[i].university);
-  };
-};
-console.log(uni);
+// // /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+// const uni = [];
+// for (i = 0; i < graduates.length; i++) {
+//   if (graduates[i].university.toLowerCase().includes('Uni')) {
+//       uni.push(graduates[i].university);
+//   };
+// };
+// console.log(uni);
 
 
 
@@ -126,27 +126,30 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
-zooAnimals.forEach(function(element){
-  animalNames.push(`Name: ${element.animal_name},  Scientific: ${element.scientific_name}.`);
-})
-console.log(animalNames);
+// const animalNames = [];
+// zooAnimals.forEach(function(element){
+//   animalNames.push(`Name: ${element.animal_name},  Scientific: ${element.scientific_name}.`);
+// })
+// console.log(animalNames);
 
-/* Request 2: .map()    
+// /* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-*/
+// The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+// */
 
-const lowerCase = [];
-
-console.log(lowerCase); 
+// const lowerCase = [];
+// zooAnimals.map(function(item){
+//   return lowerCase.push(item.animal_name.toLowerCase());
+// })
+// console.log(lowerCase); 
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
 const lowerPopulation = [];
+
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
@@ -154,7 +157,11 @@ console.log(lowerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = [];
+zooAnimals.reduce(function(totalAnmimals, item){
+  return totalAnmimals + item.population;
+}, 0);
+
 console.log(populationTotal);
 
 
