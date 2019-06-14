50fb45f79c8068670be96b9f5477a790d8988166
Tracie T <traciecodes@gmail.com>
2019-06-14 11:43:30 -0500

    Arrays section complete.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,52 +7,52 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-const tyrannosaurus = {
-  objectName: "Tyrannosaurus",
-  diet: "carnivorous",
-  weight: "7000kg",
-  length: "12m",
-  period: "Late Cretaceous",
-  roar: function(){
-    return "RAWERSRARARWERSARARARRRR";
-  }
-}
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-const stegosaurus = {
-  objectName: "Stegosaurus",
-  diet: "herbivorous",
-  weight: "2000kg",
-  length: "9m",
-  period: "Late Jurassic"
-}
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-const velociraptor = {
-  objectName: "Velociraptor",
-  diet: "carnivorous",
-  weight: "15kg",
-  length: "1.8m",
-  period: "Late Cretaceous"
-}
-// Using your dinosaur objects, log answers to these questions:
+// const tyrannosaurus = {
+//   objectName: "Tyrannosaurus",
+//   diet: "carnivorous",
+//   weight: "7000kg",
+//   length: "12m",
+//   period: "Late Cretaceous",
+//   roar: function(){
+//     return "RAWERSRARARWERSARARARRRR";
+//   }
+// }
+// // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+// const stegosaurus = {
+//   objectName: "Stegosaurus",
+//   diet: "herbivorous",
+//   weight: "2000kg",
+//   length: "9m",
+//   period: "Late Jurassic"
+// }
+// // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+// const velociraptor = {
+//   objectName: "Velociraptor",
+//   diet: "carnivorous",
+//   weight: "15kg",
+//   length: "1.8m",
+//   period: "Late Cretaceous"
+// }
+// // Using your dinosaur objects, log answers to these questions:
 
-// How much did tyrannosaurus weigh?
-console.log(tyrannosaurus.weight);
+// // How much did tyrannosaurus weigh?
+// console.log(tyrannosaurus.weight);
 
-// What was the diet of a velociraptor?
-console.log(velociraptor.diet);
+// // What was the diet of a velociraptor?
+// console.log(velociraptor.diet);
 
-// How long was a stegosaurus?
-console.log(stegosaurus.length);
+// // How long was a stegosaurus?
+// console.log(stegosaurus.length);
 
-// What time period did tyrannosaurus live in?
-console.log(tyrannosaurus.period);
-
-
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(tyrannosaurus.roar());
+// // What time period did tyrannosaurus live in?
+// console.log(tyrannosaurus.period);
 
 
-// ==== Arrays ====
+// // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+// console.log(tyrannosaurus.roar());
+
+
+// ==== Arrays ==== DONE 
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
@@ -70,8 +70,15 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
-console.log(universities)
+
+for (i = 0; i < graduates.length; i++) {
+  graduates[i].university;
+  universities.push(graduates[i].university);
+  universities.sort();
+};
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -80,12 +87,22 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
-/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+// /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.toLowerCase().includes('Uni')) {
+      uni.push(graduates[i].university);
+  };
+};
 console.log(uni);
+
+
 
 
 // ==== ADVANCED Array Methods ====
@@ -110,15 +127,18 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(function(element){
+  animalNames.push(`Name: ${element.animal_name},  Scientific: ${element.scientific_name}.`);
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
 
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-
 */
 
 const lowerCase = [];
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
